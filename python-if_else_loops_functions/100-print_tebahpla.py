3cb4f129b45196c4816a9e575ef2b75f1254d1b3
AliyevaMadina <mdn.alyv@gmail.com>
2024-09-08 16:20:09 +0400

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for x in range(122, 96, -1):
     if x % 2 == 0:
         print("{}".format(chr(x)), end="")
     else:
-        print("{}".format(chr(x) - 32), end="")
+        print("{}".format(chr(x - 32), end="")
