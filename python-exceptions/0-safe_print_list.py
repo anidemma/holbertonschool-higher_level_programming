917303f73a832fe6ceed8bf938e5b8d5f64c9953
AliyevaMadina <mdn.alyv@gmail.com>
2024-09-15 17:24:32 +0400

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-        print()
-        return x
-    except (TypeError, IndexError):
-        print()
-        return 
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
