3a6f809e23e5874259ec3a0f4019e0c9b92d2012
AliyevaMadina <mdn.alyv@gmail.com>
2024-09-22 11:55:39 +0400

    9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -13,20 +13,12 @@ class Rectangle:
         type(self).number_of_instances += 1
 
     @property
-    def height(self):
-        return self.__height
-
-    @property
     def width(self):
         return self.__width
 
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    @property
+    def height(self):
+        return self.__height
 
     @width.setter
     def width(self, value):
@@ -35,6 +27,14 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         return self.height * self.width
