948c148527b66810915380bc9c63787ef84b5e5f
AliyevaMadina <mdn.alyv@gmail.com>
2024-09-30 16:38:02 +0400

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""read a text file"""
+
+
 def read_file(filename=""):
+    """read a  text file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
+
