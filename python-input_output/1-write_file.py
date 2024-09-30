1e0b2eff115e7d2140f68f537500a5d34b4e10c2
AliyevaMadina <mdn.alyv@gmail.com>
2024-09-30 17:07:23 +0400

    2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """write a string to a text file"""
-    with open(filename, "a+", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """ appends a string at the end of a text file """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "a+", encoding="utf-8") as f:
         return f.write(text)
