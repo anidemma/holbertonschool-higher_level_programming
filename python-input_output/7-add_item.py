797dccfcc177083e568662c7db65a144f1aa29a7
AliyevaMadina <mdn.alyv@gmail.com>
2024-10-01 12:50:17 +0400

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,8 @@
 """ writes an Object to a text file, using a JSON representation"""
 from sys import argv as words
 
-
-from 5-save_to_json_file.py import save_to_json_file
-from 6-load_from_json_file.py import load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 filename = "add_item.json"
 
 try:
