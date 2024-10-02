81dd8cdf1ebc95e128e65add2e21aa962e0e7f50
AliyevaMadina <mdn.alyv@gmail.com>
2024-10-02 21:50:33 +0400

    11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(type(x) is str for x in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+         for a, b in json.items():
+            setattr(self, a, b)
