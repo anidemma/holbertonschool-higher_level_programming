b74190c6f686914a707cb666fb98c999b9c73275
AliyevaMadina <mdn.alyv@gmail.com>
2024-10-19 21:45:34 +0400

    Consuming and processing data from an API using Python

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,24 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    request = requests.get("https://jsonplaceholder.typicode.com/posts")
+    posts = request.json()
+    
+    if request.status_code == 200:
+        for post in posts:
+            print(post["title"])
+        print(request.status_code)
+
+def fetch_and_save_posts():
+    request = requests.get("https://jsonplaceholder.typicode.com/posts")
+    posts = request.json()
+
+    if request.status_code == 200:
+        with open('file2.csv', 'w', newline='') as f:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for post in posts:
+                writer.writerow({'id': post['id'], 'title': post['title'], 'body': post['body']})
+        print(request.status_code)
