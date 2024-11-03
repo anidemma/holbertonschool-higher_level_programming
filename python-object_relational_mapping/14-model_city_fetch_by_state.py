6ca1524562093e35db3ef95f2ab3e7ee2c6afeca
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-03 19:46:05 +0400

    14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Cities in state"""
+""" Cities in state"""
 
 import sys
 
@@ -10,17 +10,13 @@ from model_city import City
 from model_state import State
 
 if __name__ == "__main__":
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    query = session.query(City, State).join(State)
-
-    for _c, _s in query.all():
-        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
-
-    session.commit()
-    session.close()
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
