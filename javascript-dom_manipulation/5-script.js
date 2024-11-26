ccc7d6b13d648d53859c5fdc0d4e89e2cfb95235
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-26 21:28:38 +0400

    5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+let header = document.querySelector('header');
+let update_geader = document.querySelector('#update_header');
+
+header.addEventListener('click', () => {header.innerText = 'New Header!!!';})
