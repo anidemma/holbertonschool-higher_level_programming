8a69f0d68f67ddd9c060a7ee1e61dc894a529c79
AliyevaMadina <mdn.alyv@gmail.com>
2024-11-26 21:15:06 +0400

    1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+let header = document.querySelector('header');
+let red_header = document.querySelector('#red_header');
+
+red_header.addEventListener('click', () => {header.style.color = '#FF0000';})
